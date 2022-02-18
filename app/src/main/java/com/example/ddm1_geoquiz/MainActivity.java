ede3c864fb2d6c39592f6765eb1dc5a8772eb930
guillemmiro <guillem.miro@students.salle.url.edu>
2022-02-18 14:05:18 +0100

    Implemented different questions.

--- app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
+++ app/src/main/java/com/example/ddm1_geoquiz/MainActivity.java
@@ -6,22 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+
+    private String[] questions;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextView = (TextView) findViewById(R.id.textview1);
+
+        questions = getResources().getStringArray(R.array.questions);
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                setQuestions();
             }
         });
 
@@ -30,7 +41,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                setQuestions();
             }
         }));
+    }
+
+    void setQuestions() {
+        int randomIndex = new Random().nextInt(questions.length);
+        String question = questions[randomIndex];
+
+        mTextView.setText(question);
     }
 }
